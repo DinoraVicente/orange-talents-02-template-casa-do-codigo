2158b365d9652004256d1a0b69852b13767d045a
Dinorá Vicente <dondi8181@gmail.com>
2021-02-25 12:19:18 -0300

    validacao de estados por paises

--- src/main/java/br/com/zup/casaDoCodigo/paiseestado/EstadoController.java
+++ src/main/java/br/com/zup/casaDoCodigo/paiseestado/EstadoController.java
@@ -1,13 +1,18 @@
 package br.com.zup.casaDoCodigo.paiseestado;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class EstadoController {
@@ -19,8 +24,36 @@ public class EstadoController {
 	@Transactional
 	public String criaEstado(@RequestBody @Valid EstadoForm form) {
 		Estado estado = form.toModel(manager);
-		manager.persist(estado);
 		
-		return estado.toString();
+		Query query = manager.createQuery("select e from Estado e where e.pais.id = :id and e.nome = :nome");
+		query.setParameter("id", form.getIdPais());
+		query.setParameter("nome", form.getNome());
+		List<?> resultList = query.getResultList();
+		
+		if(resultList.size() >= 1) {
+		    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Esse estado já foi cadastrado nesse país!");
+		} else {
+			manager.persist(estado);
+			return estado.toString();
+		}
 	}
 }
+
+//@PostMapping
+//@Transactional
+//public String store(@RequestBody @Valid  StateRequest request) {
+//    // state, country_id
+//    State state = request.toState(em);
+//
+//    Query query = em.createQuery("select s from State s where s.country.id = :fk and s.name = :name");
+//    query.setParameter("fk", request.getCountryId());
+//    query.setParameter("name", request.getName());
+//    List resultList = query.getResultList();
+//
+//    if(resultList.size() >= 1) {
+//        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "This state has been already registered for this country");
+//    }
+//
+//    em.persist(state);
+//    return state.toString();
+//}
